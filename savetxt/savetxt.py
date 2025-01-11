95f2671eed448808791bfeaec35c9358c30755df
AND2797 <aditya.das2797@gmail.com>
2025-01-11 18:56:56 +0000

    use pandas to save/load data

--- savetxt/savetxt.py
+++ savetxt/savetxt.py
@@ -1,6 +1,7 @@
 import click
 from pathlib import Path
 import csv
+import pandas as pd
 import subprocess
 
 
@@ -12,8 +13,8 @@ def cli():
 @click.command()
 @click.argument('file')
 @click.argument('key')
-@click.argument('value')
 @click.argument('tags', default='')
+@click.argument('value')
 def put(file: str, key: str, value: str, tags=''):
     """
 
@@ -24,11 +25,14 @@ def put(file: str, key: str, value: str, tags=''):
     :return:
     """
     home = Path.joinpath(Path.home(), 'savetxt')
+    header = ["key", "tags", "value"]
     Path(home).mkdir(exist_ok=True)
     filepath = Path.joinpath(home, f'{file}.csv')
-    with open(filepath, 'a') as csvfile:
-        writer = csv.writer(csvfile, delimiter='|')
-        writer.writerow([key, value, tags])
+    row_to_add = pd.DataFrame(columns=header, data=[[file, key, value]])
+    if filepath.exists():
+        row_to_add.to_csv(filepath, header=False, mode='a', index=False, sep='|')
+    else:
+        row_to_add.to_csv(filepath, index=None)
 
 
 @click.command()
@@ -36,27 +40,9 @@ def put(file: str, key: str, value: str, tags=''):
 def cat(file: str):
     home = Path.joinpath(Path.home(), 'savetxt')
     filepath = Path.joinpath(home, f'{file}.csv')
-    with open(filepath, 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter='|')
-        for row in reader:
-            print(row)
-
-
-@click.command()
-@click.argument('file')
-def browse(file: str):
-    """
-    :param file: filename
-    :return:
-
-    this function pipes the contents of the csv file to fzf for fuzzy searching the file.
-    """
-    home = Path.joinpath(Path.home(), 'savetxt')
-    filepath = Path.joinpath(home, f'{file}.csv')
-    command = f'cat {filepath} | fzf'
-    result = subprocess.run(command, shell=True, capture_output=True)
-    line = result.stdout.strip().decode('utf-8')
-    print(get_site(line))
+    if filepath.exists():
+        data = pd.read_csv(filepath)
+        print(data)
 
 
 def get_site(row):
@@ -65,11 +51,6 @@ def get_site(row):
 
 cli.add_command(put)
 cli.add_command(cat)
-cli.add_command(browse)
-
-#TODO: prune functionality to remove duplicates
-#TODO: functionality to add how many times link was accessed
-#TODO: functionality to add when link was last accessed
 
 if __name__ == '__main__':
     cli()
